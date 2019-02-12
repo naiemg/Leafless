05945573420ea25897562a558b560807d38bef15
naiemg <35549126+naiemg@users.noreply.github.com>
2019-02-12 17:24:31 -0500

    Add files via upload
    Updated. 
    2-12-2019
    5:23 PM

---
+++ Leaves Outlook UPDATED/setup.sh
@@ -0,0 +1,5 @@
+# Add shell commands here to configure your environment automatically
+# Note that this script will be running in parallel with the notebook server
+# Run "cat /tmp/setup.log" in the notebook to check setup progress
+
+# pip install scikit-learn
