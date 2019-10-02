6cd6be3b3d01457678870d9c940386141406809d
Naiem Gafar <naiemgafar@Naiems-MBP.home>
2019-10-01 21:32:32 -0400

    cleared out files

--- Leaves Outlook UPDATED/setup.sh
+++
@@ -1,5 +0,0 @@
-# Add shell commands here to configure your environment automatically
-# Note that this script will be running in parallel with the notebook server
-# Run "cat /tmp/setup.log" in the notebook to check setup progress
-
-# pip install scikit-learn
